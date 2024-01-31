63a8236bf739ba256785273406e6241b6a7a842c
adithapathiraja <adithapraween@gmail.com>
2024-01-31 15:09:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 from datetime import datetime
 
-!git clone https://github.com/adithapathiraja/Solar-Power-Data-Visualization.git
-%cd /content/Solar-Power-Data-Visualization
-
-excel_file_path = '/content/Solar-Power-Data-Visualization/Dataset - Nov 01 - Dec 01 2023.xlsx'  # Replace with the path to your Excel file
+excel_file_path = 'Dataset - Nov 01 - Dec 01 2023.xlsx'  # Replace with the path to your Excel file
 df = pd.read_excel(excel_file_path)
 st.write(df)
